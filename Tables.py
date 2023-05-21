1a68ce762eb5d09431f6e7a78d64ed5b4f132a7d
nevis3 <svenhelbig3@gmail.com>
2023-05-21 15:51:20 +0200

    added one initialisation of tables

---
+++ Tables.py
@@ -0,0 +1,81 @@
+#only needs to run once. creates 2 databases, dont know why
+
+import psycopg2
+
+def Tables():
+    conn = psycopg2.connect(
+        host="localhost",
+        database="postgres",
+        user="postgres",
+        port="5432",
+        password="asdf")
+    # Creating a cursor object using the cursor() method
+    cursor = conn.cursor()
+
+    sql_localities = """CREATE TABLE localities(
+       name VARCHAR(255) PRIMARY KEY,
+       country VARCHAR(255) NOT NULL
+    )"""
+
+    cursor.execute(sql_localities)
+
+    sql_rock_types = """CREATE TABLE rock_types(
+        id VARCHAR(255) PRIMARY KEY,
+        name VARCHAR(255),
+        type VARCHAR(255),
+        age VARCHAR(255),
+        strike FLOAT,
+        dip FLOAT,
+        dip_direction VARCHAR(255)
+        )"""
+
+    cursor.execute(sql_rock_types)
+
+    sql_elements = """CREATE TABLE elements(
+        name VARCHAR(255) PRIMARY KEY,
+        symbol VARCHAR(255) NOT NULL,
+        weight FLOAT,
+        state VARCHAR(255)
+    )"""
+
+    cursor.execute(sql_elements)
+
+    sql_samples = """CREATE TABLE samples(
+       id VARCHAR(255) PRIMARY KEY,
+       rock_type VARCHAR(255) NOT NULL,
+       locality_name VARCHAR(255) NOT NULL,
+       coordinates VARCHAR(255),
+       date date
+    )"""
+
+    cursor.execute(sql_samples)
+
+    sql_locality_rock_types = """CREATE TABLE locality_rock_types(
+        locality_name VARCHAR(255) PRIMARY KEY,
+        rock_type_id VARCHAR(255),
+        FOREIGN KEY (locality_name) REFERENCES localities(name),
+        FOREIGN KEY (rock_type_id) REFERENCES rock_types(id)
+        )"""
+
+    cursor.execute(sql_locality_rock_types)
+
+
+    sql_samples_chemical_data = """CREATE TABLE samples_chemical_data(
+        sample_id VARCHAR(255) PRIMARY KEY,
+        element_name VARCHAR(255),
+        weight_percentage FLOAT,
+        FOREIGN KEY (element_name) REFERENCES elements(name),
+        FOREIGN KEY (sample_id) REFERENCES samples(id)
+        )"""
+
+    cursor.execute(sql_samples_chemical_data)
+
+
+    # Commit your changes in the database
+    conn.commit()
+
+    # Closing the connection
+    conn.close()
+
+
+Tables()
--- main.py
+++ main.py
@@ -1,21 +1,52 @@
 import psycopg2
 
-conn = psycopg2.connect(
-    host = "localhost",
-    database="postgres",
-    user="postgres",
-    password="asdf"
-)
+def add_locality(input_data):
+    # creating a cursor object
+    cursor = conn.cursor()
+
+    # inserting record into employee table
+    for d in input_data:
+        cursor.execute("INSERT into localities(name, country) VALUES (%s,%s)", d)
+
+    #print("List has been inserted to employee table successfully...")
 
 
+def fetch_data(database_info):
+    # creating a cursor object
+    cursor = conn.cursor()
+    sql2 = "select * from %s" %database_info
+    # executing query
+    cursor.execute(sql2)
+
+    # fetching the result
+    print(cursor.fetchall())
+
+def delete_entries(input_data):
+    cursor = conn.cursor()
+    sql = "DELETE FROM localities WHERE country = '%s'" %input_data
+
+    cursor.execute(sql)
 
 if __name__ == '__main__':
-    cur = conn.cursor()
-    query = "SELECT * FROM teaches NATURAL JOIN attends NATURAL JOIN likes"
-    cur.execute(query)
-    data = cur.fetchall()
-    conn.commit()
-    for row in data:
-        print(row)
+    conn = psycopg2.connect(
+        host="localhost",
+        database="postgres",
+        user="postgres",
+        port="5432",
+        password="asdf")
 
+    data = [('Friesland', 'Germany'), ('Grassland', 'Germany')]
+    add_locality(data)
+
+    fetch_data("localities")
+
+    delete_entries('Germany')
+
+    fetch_data("localities")
+
+    # Commit your changes in the database
+    conn.commit()
+
+    # Closing the connection
     conn.close()
+
