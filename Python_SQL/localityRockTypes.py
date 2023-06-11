915f8e75d138eaeadbe973a863cdab60054872c2
PhilipMandas <philipmandas@gmail.com>
2023-06-11 18:49:50 +0200

    Changed some data

--- Python_SQL/localityRockTypes.py
+++ Python_SQL/localityRockTypes.py
@@ -14,14 +14,14 @@ def LocalityRockTypes():
     cursor = conn.cursor()
 
     sql_locality_rock_types_data = """INSERT INTO locality_rock_types (locality_name, rock_type_id) VALUES
-        ('South Dakota', '1a'), 
-        ('British Isles', '1b'), 
-        ('Southwest USA', '1c'), 
-        ('Upper Peninsula', '1d'), 
-        ('Andes Mountains', '2a'), 
-        ('Australia', '2b'), 
-        ('Amazon Rainforest', '2c'), 
-        ('Southern Africa', '2d');"""
+        ('South Dakota', 'A'), 
+        ('British Isles', 'B'), 
+        ('Southwest USA', 'C'), 
+        ('Upper Peninsula', 'D'), 
+        ('Andes Mountains', 'E'), 
+        ('Australia', 'F'), 
+        ('Amazon Rainforest', 'G'), 
+        ('Southern Africa', 'H');"""
     
     cursor.execute(sql_locality_rock_types_data)
 
